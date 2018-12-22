5182caf527d2ceccb40620eff5f584819575c5a4
Mariusz Trela <mariusztrela01@gmail.com>
2018-12-22 17:02:32 +0100

    Three.js test

--- index.php
+++
@@ -1,8 +0,0 @@
-<html><head><meta charset="utf-8">
-<title>Fictional town</title>
-</head><body><center><h3>Your fictional town is:</h3><h1>
-<?php
-echo exec('./markov_text_generator')
-?>
-</h1></center></body></html>
-
